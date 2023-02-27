c98c944de2ca9cc0dc435f6a88c5cde5e7d10795
Anivaries <k0m4r4c00@gmail.com>
2023-02-27 18:05:26 +0100

    update

--- elden_ring_lore/settings.py
+++ elden_ring_lore/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['https://erlore-website.herokuapp.com/']
+ALLOWED_HOSTS = ['eldenlore.herokuapp.com/']
 
 # Application definition
 
